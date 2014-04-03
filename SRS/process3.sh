154bb3086f847214c9f534ed32f1fb8d2aa63a61
Rauf Aliev <aliev@teamidea.ru>
2014-04-03 10:13:21 +0400

    title page & headers

--- SRS/process3.sh
+++ SRS/process3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cat requirements.csv | perl -i -npe "s/\"\t\"//g" | perl -i -npe "s/\"\t//g" | perl -i -npe "s/\t\"//g" | perl -i -npe "s/\t//g" | perl -i -npe "s/\"\"/\"/g" | perl -i -npe "s/([\t ])\"/\1<</g" | perl -i -npe "s/\"([\t \.\,])/>>\1/g" > a.csv
-cat  a.csv | grep "{1 этап}" | grep "На согласование"  > stage1.tobeagreed.csv 
-cat  a.csv | grep "{1 этап}" | grep -v "На согласование"  > stage1.csv 
+export ZAM="В объеме доп.согл. № 2"
+cat requirements.csv | perl -i -npe "s/\"\t\"//g" | perl -i -npe "s/1 этап/$ZAM/g" | perl -i -npe "s/\"\t//g" | perl -i -npe "s/\t\"//g" | perl -i -npe "s/\t//g" | perl -i -npe "s/\"\"/\"/g" | perl -i -npe "s/([\t ])\"/\1<</g" | perl -i -npe "s/\"([\t \.\,])/>>\1/g" > a.csv
+cat  a.csv | grep "{$ZAM}" | grep "На согласование" |perl -i -npe "s/№/\\\No/g" > stage1.tobeagreed.csv 
+cat  a.csv | grep "{$ZAM}" | grep -v "На согласование" |perl -i -npe "s/№/\\\No/g" > stage1.csv 
 cat  a.csv | grep "{2 этап}"  > stage2.csv 
 iconv -f cp1251 -t utf-8 stage1.csv  > stage1.utf8.csv
 iconv -f cp1251 -t utf-8 stage2.csv  > stage2.utf8.csv
