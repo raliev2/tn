7e91659c4fdae3dea824998c77c84d33a348a0a2
Kolotushkina Larisa <kolotushkina@teamidea.ru>
2014-05-16 12:30:47 +0400

    changes

---
+++ hybris/technonikol_511/b2btechnonikol/b2btechnonikolservices/src/com/teamidea/platform/technonikol/services/constants/B2btechnonikolservicesConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package com.teamidea.platform.technonikol.services.constants;
+
+/**
+ * Global class for all B2btechnonikolservices constants. You can add global constants for your extension into this class.
+ */
+public final class B2btechnonikolservicesConstants extends GeneratedB2btechnonikolservicesConstants
+{
+	public static final String EXTENSIONNAME = "b2btechnonikolservices";
+
+	private B2btechnonikolservicesConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
