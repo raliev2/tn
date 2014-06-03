3cc439994f87ceb53921e9af9b6b41dd42e9735f
Alexey Lubimov <lubimov.alexey@gmail.com>
2014-06-04 00:42:13 +0400

    header autorization links; fix items; fix b2b price provider;

--- hybris/technonikol_511/b2btechnonikol/b2btechnonikolcore/src/com/teamidea/platform/technonikol/core/pricing/TNB2BUnitService.java
+++ hybris/technonikol_511/b2btechnonikol/b2btechnonikolcore/src/com/teamidea/platform/technonikol/core/pricing/TNB2BUnitService.java
@@ -1,65 +1,188 @@
 package com.teamidea.platform.technonikol.core.pricing;
 
-import com.google.common.collect.Lists;
 import de.hybris.platform.b2b.constants.B2BConstants;
 import de.hybris.platform.b2b.model.B2BCustomerModel;
 import de.hybris.platform.b2b.model.B2BUnitModel;
-import de.hybris.platform.b2b.model.B2BUserGroupModel;
 import de.hybris.platform.b2b.services.impl.DefaultB2BUnitService;
 import de.hybris.platform.core.model.enumeration.EnumerationValueModel;
 import de.hybris.platform.core.model.security.PrincipalGroupModel;
 import de.hybris.platform.core.model.user.UserGroupModel;
 import de.hybris.platform.core.model.user.UserModel;
 import de.hybris.platform.europe1.constants.Europe1Constants;
+import de.hybris.platform.europe1.enums.UserPriceGroup;
 import de.hybris.platform.servicelayer.session.Session;
 import de.hybris.platform.servicelayer.session.SessionExecutionBody;
-import org.apache.commons.collections.CollectionUtils;
 
 import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.teamidea.platform.technonikol.core.constants.B2btechnonikolCoreConstants;
+
 
 /**
  * Created by anton.gavazyuk on 07/04/14.
  */
-public class TNB2BUnitService extends DefaultB2BUnitService {
+public class TNB2BUnitService extends DefaultB2BUnitService
+{
+	private static final Logger log = Logger.getLogger(TNB2BUnitService.class);
 
-    @Override
-    public void updateBranchInSession(final Session session, final UserModel currentUser)
-    {
-        if (currentUser instanceof B2BCustomerModel)
-        {
-            final Collection<Long> priceLists = Lists.newArrayList();
-            final Object[] branchInfo = (Object[]) getSessionService().executeInLocalView(new SessionExecutionBody()
-            {
-                @Override
-                public Object[] execute()
-                {
-                    getSearchRestrictionService().disableSearchRestrictions();
-                    final B2BCustomerModel currentCustomer = (B2BCustomerModel) currentUser;
-                    final B2BUnitModel unitOfCustomer = getParent(currentCustomer);
+	@Override
+	public void updateBranchInSession(final Session session, final UserModel currentUser)
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug(">> updateBranchInSession()");
+		}
 
-                    Collection<PrincipalGroupModel> userGroupModels = unitOfCustomer.getAllGroups();
+		if (currentUser instanceof B2BCustomerModel)
+		{
+			final Collection<Long> priceLists = Sets.newHashSet();
 
-                    for(PrincipalGroupModel b2bUnitOrganization: userGroupModels){
-                        if(b2bUnitOrganization instanceof UserGroupModel && ((UserGroupModel) b2bUnitOrganization).getUserPriceGroup()!=null)
-                            priceLists.add(getTypeService().getEnumerationValue(((UserGroupModel) b2bUnitOrganization).getUserPriceGroup()).getPk().getLongValue());
-                    }
-                    final EnumerationValueModel userPriceGroup = (unitOfCustomer.getUserPriceGroup() != null ? getTypeService()
-                            .getEnumerationValue(unitOfCustomer.getUserPriceGroup()) : lookupPriceGroupFromClosestParent(unitOfCustomer));
+			final Object[] branchInfo = (Object[]) getSessionService().executeInLocalView(new SessionExecutionBody()
+			{
+				@Override
+				public Object[] execute()
+				{
+					getSearchRestrictionService().disableSearchRestrictions();
 
-                    priceLists.add(userPriceGroup.getPk().getLong());
-                    return new Object[]
-                            { getRootUnit(unitOfCustomer), getBranch(unitOfCustomer), unitOfCustomer, userPriceGroup };
-                }
-            });
+					final B2BCustomerModel currentCustomer = (B2BCustomerModel) currentUser;
+					final B2BUnitModel unitOfCustomer = getParent(currentCustomer);
 
-            getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_ROOTUNIT, branchInfo[0]);
-            getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_BRANCH, branchInfo[1]);
-            getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_UNIT, branchInfo[2]);
-            if(CollectionUtils.isNotEmpty(priceLists)){
-                getSessionService().setAttribute("PRICE_LISTS",priceLists);
-            } else {
-                getSessionService().setAttribute(Europe1Constants.PARAMS.UPG, branchInfo[3]);
-            }
-        }
-    }
+					// Will not add UPG from groups only their own and parents UPG will be used
+					//addAllGroupsUPG(priceLists, unitOfCustomer);					
+
+					if (unitOfCustomer.getAllUserPriceGroups() != null && !unitOfCustomer.getAllUserPriceGroups().isEmpty())
+					{
+						addAllUPG(priceLists, asEnumerationValueModels(unitOfCustomer.getAllUserPriceGroups()));
+					}
+					else
+					{
+						addAllUPG(priceLists, lookupPriceGroupCollectionFromClosestParent(unitOfCustomer));
+					}
+
+					final EnumerationValueModel userPriceGroup = (unitOfCustomer.getUserPriceGroup() != null ? getTypeService()
+							.getEnumerationValue(unitOfCustomer.getUserPriceGroup()) : lookupPriceGroupFromClosestParent(unitOfCustomer));
+
+					priceLists.add(userPriceGroup.getPk().getLong());
+
+					return new Object[]
+					{ getRootUnit(unitOfCustomer), getBranch(unitOfCustomer), unitOfCustomer, userPriceGroup };
+				}
+
+			});
+
+			getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_ROOTUNIT, branchInfo[0]);
+			getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_BRANCH, branchInfo[1]);
+			getSessionService().setAttribute(B2BConstants.CTX_ATTRIBUTE_UNIT, branchInfo[2]);
+			if (CollectionUtils.isNotEmpty(priceLists))
+			{
+				getSessionService().setAttribute(B2btechnonikolCoreConstants.CTX_ATTRIBUTE_PRICE_LISTS, priceLists);
+			}
+			else
+			{
+				getSessionService().setAttribute(Europe1Constants.PARAMS.UPG, branchInfo[3]);
+			}
+		}
+
+		if (log.isDebugEnabled())
+		{
+			log.debug("<< updateBranchInSession()");
+		}
+	}
+
+
+	/**
+	 * Add the UPG from the assigned groups
+	 * 
+	 * @param priceLists
+	 * @param unitOfCustomer
+	 */
+	private void addAllGroupsUPG(final Collection<Long> priceLists, final B2BUnitModel unitOfCustomer)
+	{
+		final Collection<PrincipalGroupModel> userGroupModels = unitOfCustomer.getAllGroups();
+
+		for (final PrincipalGroupModel b2bUnitOrganization : userGroupModels)
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("Check user price groups for B2BUnit: " + b2bUnitOrganization.getUid());
+			}
+
+			if (b2bUnitOrganization instanceof UserGroupModel)
+			{
+				addAllUPG(priceLists, asEnumerationValueModels(((UserGroupModel) b2bUnitOrganization).getAllUserPriceGroups()));
+			}
+		}
+	}
+
+	/**
+	 * Add all UPG
+	 * 
+	 * @param priceLists
+	 * @param userGroup
+	 */
+	private void addAllUPG(final Collection<Long> priceLists, final Collection<EnumerationValueModel> upgModels)
+	{
+		for (final EnumerationValueModel model : upgModels)
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("add UPG: " + model.getCode());
+			}
+			priceLists.add(model.getPk().getLongValue());
+		}
+	}
+
+	/**
+	 * 
+	 * @param userPriceGroups
+	 * @return
+	 */
+	private Collection<EnumerationValueModel> asEnumerationValueModels(final Collection<UserPriceGroup> userPriceGroups)
+	{
+		final List<EnumerationValueModel> enumModels = Lists.newArrayList();
+
+		for (final UserPriceGroup priceGroup : userPriceGroups)
+		{
+			enumModels.add(getTypeService().getEnumerationValue(priceGroup));
+		}
+		return enumModels;
+	}
+
+	/**
+	 * 
+	 * @param unitOfCustomer
+	 * @return
+	 */
+	protected Collection<EnumerationValueModel> lookupPriceGroupCollectionFromClosestParent(final B2BUnitModel unitOfCustomer)
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug(">> lookupPriceGroupCollectionFromClosestParent(). B2BUnit:" + unitOfCustomer.getUid());
+		}
+
+		for (final B2BUnitModel unitModel : getAllParents(unitOfCustomer))
+		{
+
+			if (unitModel.getAllUserPriceGroups() != null && !unitModel.getAllUserPriceGroups().isEmpty())
+			{
+				if (log.isDebugEnabled())
+				{
+					log.debug("<< lookupPriceGroupCollectionFromClosestParent()");
+				}
+				return asEnumerationValueModels(unitModel.getAllUserPriceGroups());
+			}
+		}
+
+		if (log.isDebugEnabled())
+		{
+			log.debug("<< lookupPriceGroupCollectionFromClosestParent().B2B_DEFAULT_PRICE_GROUP");
+		}
+		return Lists.newArrayList(getTypeService().getEnumerationValue("UserPriceGroup", "B2B_DEFAULT_PRICE_GROUP"));
+	}
 }
--- hybris/technonikol_511/b2btechnonikol/b2btechnonikolcore/src/com/teamidea/platform/technonikol/core/pricing/TNPriceFactory.java
+++ hybris/technonikol_511/b2btechnonikol/b2btechnonikolcore/src/com/teamidea/platform/technonikol/core/pricing/TNPriceFactory.java
@@ -1,8 +1,5 @@
 package com.teamidea.platform.technonikol.core.pricing;
 
-import com.google.common.collect.Collections2;
-import com.google.common.collect.Lists;
-import de.hybris.platform.b2bacceleratorservices.company.B2BCommerceB2BUserGroupService;
 import de.hybris.platform.catalog.jalo.CatalogAwareEurope1PriceFactory;
 import de.hybris.platform.core.PK;
 import de.hybris.platform.europe1.constants.Europe1Constants;
@@ -15,173 +12,221 @@ import de.hybris.platform.jalo.c2l.C2LManager;
 import de.hybris.platform.jalo.c2l.Currency;
 import de.hybris.platform.jalo.enumeration.EnumerationValue;
 import de.hybris.platform.jalo.flexiblesearch.FlexibleSearch;
-import de.hybris.platform.jalo.order.price.JaloPriceFactoryException;
 import de.hybris.platform.jalo.product.Product;
-import de.hybris.platform.jalo.product.Unit;
 import de.hybris.platform.jalo.user.User;
 import de.hybris.platform.servicelayer.search.FlexibleSearchService;
-import de.hybris.platform.servicelayer.user.UserService;
 import de.hybris.platform.util.DateRange;
-import org.apache.commons.collections.CollectionUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.log4j.Logger;
+
+import com.teamidea.platform.technonikol.core.constants.B2btechnonikolCoreConstants;
+
 
 /**
  * Created by anton.gavazyuk on 07/04/14.
  */
-public class TNPriceFactory extends CatalogAwareEurope1PriceFactory {
+public class TNPriceFactory extends CatalogAwareEurope1PriceFactory
+{
+	private static final Logger log = Logger.getLogger(TNPriceFactory.class);
 
-    private FlexibleSearchService flexibleSearchService;
+	private FlexibleSearchService flexibleSearchService;
 
-    private UserService userService;
+	//private UserService userService;
+	//private B2BCommerceB2BUserGroupService b2BUserGroupService;
 
-    private B2BCommerceB2BUserGroupService b2BUserGroupService;
+	@Override
+	protected List<PriceRow> filterPriceRows4Info(final Collection<PriceRow> rows, final Currency curr, final Date date,
+			final PriceRowChannel channel)
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug(">> filterPriceRows4Info()");
+		}
+
+		if (rows.isEmpty())
+		{
+			return Collections.EMPTY_LIST;
+		}
+
+		final Currency base = (curr.isBase().booleanValue()) ? null : C2LManager.getInstance().getBaseCurrency();
+
+		final List<PriceRow> ret = new ArrayList<PriceRow>(rows);
+		boolean hasChannelRowMatching = false;
+		for (final ListIterator<PriceRow> it = ret.listIterator(); it.hasNext();)
+		{
+			final PriceRow priceRow = it.next();
+			final Currency currency = priceRow.getCurrency();
+			if ((!(curr.equals(currency))) && (((base == null) || (!(base.equals(currency))))))
+			{
+				it.remove();
+			}
+			else
+			{
+				final DateRange dateRange = priceRow.getDateRange();
+				if ((dateRange != null) && (!(dateRange.encloses(date))))
+				{
+					it.remove();
+				}
+				else if (priceRow.isGiveAwayPriceAsPrimitive())
+				{
+					it.remove();
+				}
+				else if ((channel != null) && (priceRow.getChannel() != null)
+						&& (!(priceRow.getChannel().getCode().equalsIgnoreCase(channel.getCode()))))
+				{
+					it.remove();
+				}
+				else
+				{
+					if ((channel == null) || (priceRow.getChannel() == null)
+							|| (!(priceRow.getChannel().getCode().equalsIgnoreCase(channel.getCode()))))
+					{
+						continue;
+					}
+					hasChannelRowMatching = true;
+				}
+			}
+		}
+
+		if ((hasChannelRowMatching) && (ret.size() > 1))
+		{
+			for (final ListIterator<PriceRow> it = ret.listIterator(); it.hasNext();)
+			{
+				final PriceRow priceRow = it.next();
+				if (priceRow.getChannel() != null)
+				{
+					continue;
+				}
+				it.remove();
+			}
+		}
+
+		if (log.isDebugEnabled())
+		{
+			log.debug("After filtering. " + ret.toString());
+		}
+
+		// find minumum price
+		double minPrice = Double.MAX_VALUE;
+		PriceRow selectedPriceRow = null;
+		PriceRow defaultPriceRow = null;
+		for (final PriceRow priceRow : ret)
+		{
+			if (priceRow.getUserGroup() != null)
+			{
+				if (minPrice > priceRow.getPrice())
+				{
+					minPrice = priceRow.getPrice();
+					selectedPriceRow = priceRow;
+				}
+			}
+			else
+			{
+				if (defaultPriceRow == null)
+				{
+					defaultPriceRow = priceRow;
+				}
+			}
+		}
+		final List<PriceRow> returnRows = new ArrayList<PriceRow>();
+		returnRows.add(selectedPriceRow != null ? selectedPriceRow : defaultPriceRow);
+
+		if (log.isDebugEnabled())
+		{
+			log.debug("minimum price: " + selectedPriceRow + " -> "
+					+ (selectedPriceRow != null ? selectedPriceRow.getPrice().doubleValue() : "-"));
+			log.debug("default price: " + defaultPriceRow + " -> "
+					+ (defaultPriceRow != null ? defaultPriceRow.getPrice().doubleValue() : "-"));
+
+			log.debug("<< filterPriceRows4Info(). " + returnRows.toString());
+		}
+
+		return returnRows;
+	}
+
+	@Override
+	protected Collection<PriceRow> queryPriceRows4Price(final SessionContext ctx, final Product product,
+			final EnumerationValue productGroup, final User user, final EnumerationValue userGroup)
+	{
+		if (log.isDebugEnabled())
+		{
+			log.debug(">> queryPriceRows4Price(). " + "Product:" + (product != null ? product.getCode() : "-") + "; User:"
+					+ (user != null ? user.getUid() : "-"));
+		}
+
+		//
+		final StringBuilder query = new StringBuilder("SELECT {").append(Item.PK).append("} FROM {")
+				.append(Europe1Constants.TC.PRICEROW).append(" AS pr} WHERE ");
+		final Map values = new HashMap();
+
+		// product & PG
+		appendProductConditions(query, "pr", product, productGroup, values, PriceRow.PG);
 
 
-    protected List<PriceRow> filterPriceRows4Info(final Collection<PriceRow> rows, final Currency curr, final Date date,
-                                                  final PriceRowChannel channel)
-    {
-        if (rows.isEmpty())
-        {
-            return Collections.EMPTY_LIST;
-        }
-        else
-        {
-            final Currency base = curr.isBase().booleanValue() ? null : C2LManager.getInstance().getBaseCurrency();
+		// user & UG
+		query.append(" AND ");
+		if (CollectionUtils.isNotEmpty((Collection) ctx.getAttribute(B2btechnonikolCoreConstants.CTX_ATTRIBUTE_PRICE_LISTS)))
+		{
+			appendUserConditions(query, (Collection) ctx.getAttribute(B2btechnonikolCoreConstants.CTX_ATTRIBUTE_PRICE_LISTS),
+					values, user);
+		}
+		else
+		{
+			appendUserConditions(query, "pr", user, userGroup, values, PriceRow.UG);
+		}
 
-            final List<PriceRow> returnRows = new ArrayList<PriceRow>();
+		//        query.append(" SORT BY {}")
 
-            boolean hasChannelRowMatching = false;
+		if (log.isDebugEnabled())
+		{
+			log.debug("<< queryPriceRows4Price(). " + "Query:" + query.toString());
+		}
 
-            double minPrice = Double.MAX_VALUE;
-            PriceRow selectedPriceRow = null;
-            PriceRow defaultPriceRow = null;
+		return FlexibleSearch.getInstance().search(//
+				ctx, //
+				query.toString(), //
+				values, //
+				PriceRow.class//
+				).getResult();
 
-            for(PriceRow priceRow: rows){
-                final Currency currency = priceRow.getCurrency();
-                if (!curr.equals(currency) && (base == null || !base.equals(currency)))
-                {
-                    continue; // skip if currency does not match
-                }
+	}
 
-                final DateRange dateRange = priceRow.getDateRange();
-                if (dateRange != null && !dateRange.encloses(date))
-                {
-                    continue; // skip if date range does not match
-                }
-                if (priceRow.isGiveAwayPriceAsPrimitive())
-                {
-                    continue; // always skip give-away prices -> do not show up in info
-                }
+	protected StringBuilder appendUserConditions(final StringBuilder query, final Collection<Long> userGroups, final Map values,
+			final User user)
+	{
+		query.append("{").append("pr").append(":").append(PDTRow.USERMATCHQUALIFIER).append("} IN ( ?anyU ");
+		values.put("anyU", Long.valueOf(PK.NULL_PK.getLongValue()));
+		if (user != null)
+		{
+			query.append(",?user ");
+			values.put("user", Long.valueOf(user.getPK().getLongValue()));
+		}
 
-                //YTODO: Need to discuss with Business. Will validate this condition tomorrow.
-				/*
-				 * if (channel == null && priceRow.getChannel() != null && priceRow.getChannel().getCode() != null) {
-				 * it.remove(); continue; // skip if the channel provided by the context is null and not the price row
-				 * channel. }
-				 */
+		for (final Long ugId : userGroups)
+		{
+			query.append(" ,").append(ugId);
+		}
+		query.append(")");
+		return query;
+	}
 
-                if (channel != null && priceRow.getChannel() != null
-                        && !priceRow.getChannel().getCode().equalsIgnoreCase(channel.getCode()))
-                {
-                    continue; // skip if the price row channel is defined and it's not matching with the channel provided by the context.
-                }
+	public FlexibleSearchService getFlexibleSearchService()
+	{
+		return flexibleSearchService;
+	}
 
-                if (channel != null && priceRow.getChannel() != null
-                        && priceRow.getChannel().getCode().equalsIgnoreCase(channel.getCode()))
-                {
-                    // found a matching row for the given channel
-                    hasChannelRowMatching = true;
-                }
-
-                if(priceRow.getPrice()<minPrice) {
-                    if(priceRow.getUserGroup()!=null) {
-                        minPrice = priceRow.getPrice();
-                        selectedPriceRow = priceRow;
-                    }else {
-                        defaultPriceRow = priceRow;
-                    }
-                }
-
-            }
-
-            if(selectedPriceRow != null){
-                returnRows.add(selectedPriceRow);
-            } else {
-                returnRows.add(defaultPriceRow);
-            }
-
-
-
-            // if there is a possibility to have price row without channel
-//            if (hasChannelRowMatching && returnRows.size() > 1)
-//            {
-//                for (final ListIterator<PriceRow> it = ret.listIterator(); it.hasNext();)
-//                {
-//                    final PriceRow priceRow = it.next();
-//                    if (priceRow.getChannel() == null)
-//                    {
-//                        it.remove();
-//                        continue; // remove price row without channel
-//                    }
-//                }
-//            }
-            return returnRows;
-        }
-    }
-
-    protected Collection<PriceRow> queryPriceRows4Price(final SessionContext ctx, final Product product,
-                                                        final EnumerationValue productGroup, final User user, final EnumerationValue userGroup)
-    {
-        //
-        final StringBuilder query = new StringBuilder("SELECT {").append(Item.PK).append("} FROM {").
-                append(Europe1Constants.TC.PRICEROW).append(" AS pr} WHERE ");
-        final Map values = new HashMap();
-
-        // product & PG
-        appendProductConditions(query, "pr", product, productGroup, values, PriceRow.PG);
-
-
-        // user & UG
-        query.append(" AND ");
-        if(CollectionUtils.isNotEmpty((Collection)ctx.getAttribute("PRICE_LISTS"))){
-            appendUserConditions(query,(Collection)ctx.getAttribute("PRICE_LISTS"),values,user);
-        } else {
-            appendUserConditions(query, "pr", user, userGroup, values, PriceRow.UG);
-        }
-
-//        query.append(" SORT BY {}")
-
-        return FlexibleSearch.getInstance().search(//
-                ctx, //
-                query.toString(), //
-                values, //
-                PriceRow.class//
-        ).getResult();
-
-    }
-
-    protected StringBuilder appendUserConditions(StringBuilder query, Collection<Long> userGroups, Map values, User user){
-        query.append("{").append("pr").append(":").append(PDTRow.USERMATCHQUALIFIER).append("} IN ( ?anyU ");
-        values.put("anyU", Long.valueOf(PK.NULL_PK.getLongValue()));
-        if (user != null)
-        {
-            query.append(",?user ");
-            values.put("user", Long.valueOf(user.getPK().getLongValue()));
-        }
-
-        for(Long ugId: userGroups){
-           query.append(",").append(ugId);
-        }
-        query.append(")");
-        return query;
-    }
-
-    public FlexibleSearchService getFlexibleSearchService() {
-        return flexibleSearchService;
-    }
-
-    public void setFlexibleSearchService(FlexibleSearchService flexibleSearchService) {
-        this.flexibleSearchService = flexibleSearchService;
-    }
+	public void setFlexibleSearchService(final FlexibleSearchService flexibleSearchService)
+	{
+		this.flexibleSearchService = flexibleSearchService;
+	}
 }
