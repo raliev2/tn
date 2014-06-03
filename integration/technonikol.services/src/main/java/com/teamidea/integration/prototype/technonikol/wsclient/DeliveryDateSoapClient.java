801dfc283a5bd0bf951d1257e0caf8353eb75382
Marina Zhigalova <Zhigalova@teamidea.ru>
2014-06-03 18:22:04 +0400

    technonikol.services DeliveryDataSoapClient integration

--- integration/technonikol.services/src/main/java/com/teamidea/integration/prototype/technonikol/wsclient/DeliveryDateSoapClient.java
+++ integration/technonikol.services/src/main/java/com/teamidea/integration/prototype/technonikol/wsclient/DeliveryDateSoapClient.java
@@ -1,5 +1,10 @@
 package com.teamidea.integration.prototype.technonikol.wsclient;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.teamidea.integration.prototype.technonikol.wsclient.demo.DeliveryDateSoapClientDemo;
+
 import ru.technonikol.ws.stocks.DeliveryDateQueryOut;
 import ru.technonikol.ws.stocks.MaterialsRow;
 import ru.technonikol.ws.stocks.PocketQuery;
@@ -7,44 +12,117 @@ import ru.technonikol.ws.stocks.PocketQuery.Materials;
 import ru.technonikol.ws.stocks.SendQuery;
 import ru.technonikol.ws.stocks.SendQueryResponse;
 
+
 public class DeliveryDateSoapClient
 {
-	
-	DeliveryDateQueryOut port;
 
-	public DeliveryDateSoapClient(DeliveryDateQueryOut port){
+	DeliveryDateQueryOut port;
+	
+	public DeliveryDateSoapClient(DeliveryDateQueryOut port)
+	{
 		this.port = port;
 	}
 
-	public SendQueryResponse deliveryDateQueryOut(String EKN, String count, String datePost){
+	/**
+	 * @param ekn
+	 *           product code
+	 * @param count
+	 *           product quantity for order
+	 * @param datePost
+	 *           product delivery date
+	 * @return information about product delivery information
+	 */
+	public SendQueryResponse deliveryDateQueryOut(String ekn, String count, String datePost)
+	{
 		SendQuery request = new SendQuery();
-			
+
+		PocketQuery pocketQuery = populatePocketQuery("", "", "", "", "", "", "", "", "", "", "", "", "", "", ekn, count, datePost);
+
+		request.setQueryMessage(pocketQuery);
+		System.out.println("Request: " + request.getQueryMessage());
+
+		SendQueryResponse response = port.deliveryDateQueryOut(request);
+		System.out.println("Response: " + response.getReturn());
+		return response;
+	}
+
+	/**
+	 * @param ekn
+	 *           product code
+	 * @param count
+	 *           product quantity for order
+	 * @param datePost
+	 *           product delivery date
+	 * @param idPartner
+	 *           client group id
+	 * @return information about product delivery information
+	 */
+	public SendQueryResponse deliveryDateQueryOut(String ekn, String count, String datePost, String idPartner)
+	{
+		SendQuery request = new SendQuery();
+
+		PocketQuery pocketQuery = populatePocketQuery("", "", "", "", "", "", "", idPartner, "", "", "", "", "", "", ekn, count,
+				datePost);
+
+		request.setQueryMessage(pocketQuery);
+		System.out.println("Request: " + request.getQueryMessage());
+
+		SendQueryResponse response = port.deliveryDateQueryOut(request);
+		System.out.println("Response: " + response.getReturn());
+		return response;
+	}
+
+	/**
+	 * @param parameters
+	 *           addressString, apartment, area, building, city, country, house, idPartner, numberOrder, postIndex, regionCode,
+	 *           regionName, street, town, ekn, count, datePost
+	 * @return information about product delivery information
+	 */
+	public SendQueryResponse deliveryDateQueryOut(String... parameters)
+	{
+		SendQuery request = new SendQuery();
+
+		PocketQuery pocketQuery = populatePocketQuery(parameters[0], parameters[1], parameters[2], parameters[3], parameters[4],
+				parameters[5], parameters[6], parameters[7], parameters[8], parameters[9], parameters[10], parameters[11],
+				parameters[12], parameters[13], parameters[14], parameters[15], parameters[16]);
+
+		request.setQueryMessage(pocketQuery);
+		System.out.println("Request: " + request.getQueryMessage());
+
+		SendQueryResponse response = port.deliveryDateQueryOut(request);
+		System.out.println("Response: " + response.getReturn());
+		return response;
+	}
+
+	public static PocketQuery populatePocketQuery(String addressString, String apartment, String area, String building,
+			String city, String country, String house, String idPartner, String numberOrder, String postIndex, String regionCode,
+			String regionName, String street, String town, String ekn, String count, String datePost)
+	{
 		PocketQuery pocketQuery = new PocketQuery();
-		//TODO fill in data
-		//pocketQuery.setAddressString("test");
-		//pocketQuery.setApartment("test");
-		//pocketQuery.setArea("test");
-		//pocketQuery.setBuilding("test");
-		//pocketQuery.setCity("test");
-		//pocketQuery.setCountry("test");
-		//pocketQuery.setHouse("test");
-		//pocketQuery.setIDPartner("test");
-		//pocketQuery.setNumberOrder("test");
-		//pocketQuery.setPostIndex("test");
-		//pocketQuery.setRegionCode("test");
-		//pocketQuery.setRegionName("test");
-		//pocketQuery.setStreet("test");
-		//pocketQuery.setTown("test");
-		
+		pocketQuery.setAddressString(addressString);
+		pocketQuery.setApartment(apartment);
+		pocketQuery.setArea(area);
+		pocketQuery.setBuilding(building);
+		pocketQuery.setCity(city);
+		pocketQuery.setCountry(country);
+		pocketQuery.setHouse(house);
+		pocketQuery.setIDPartner(idPartner);
+		pocketQuery.setNumberOrder(numberOrder);
+		pocketQuery.setPostIndex(postIndex);
+		pocketQuery.setRegionCode(regionCode);
+		pocketQuery.setRegionName(regionName);
+		pocketQuery.setStreet(street);
+		pocketQuery.setTown(town);
+
 		Materials materials = new Materials();
 		MaterialsRow row = new MaterialsRow();
-		row.setEKN(EKN);
+		row.setEKN(ekn);
 		row.setCount(count);
 		row.setDatePost(datePost);
 		materials.getRow().add(row);
 		pocketQuery.setMaterials(materials);
-				
-		return port.deliveryDateQueryOut(request);
+
+		return pocketQuery;
 	}
 
 }
