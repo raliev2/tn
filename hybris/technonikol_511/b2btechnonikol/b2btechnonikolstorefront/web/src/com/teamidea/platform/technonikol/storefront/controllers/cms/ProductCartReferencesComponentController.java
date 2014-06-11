063171469b754d9399dc4ed89475128bf2e060f6
Igor Bobko <limit-speed@yandex.ru>
2014-06-11 22:38:29 +0400

    Cool!

--- hybris/technonikol_511/b2btechnonikol/b2btechnonikolstorefront/web/src/com/teamidea/platform/technonikol/storefront/controllers/cms/ProductCartReferencesComponentController.java
+++ hybris/technonikol_511/b2btechnonikol/b2btechnonikolstorefront/web/src/com/teamidea/platform/technonikol/storefront/controllers/cms/ProductCartReferencesComponentController.java
@@ -8,27 +8,33 @@
  * ("Confidential Information"). You shall not disclose such Confidential
  * Information and shall use it only in accordance with the terms of the
  * license agreement you entered into with hybris.
- * 
- *  
+ *
+ *
  */
 package com.teamidea.platform.technonikol.storefront.controllers.cms;
 
-import com.teamidea.platform.technonikol.storefront.controllers.ControllerConstants;
 import de.hybris.platform.acceleratorcms.model.components.ProductReferencesComponentModel;
 import de.hybris.platform.b2bacceleratorfacades.order.B2BCartFacade;
+import de.hybris.platform.commercefacades.order.data.OrderEntryData;
 import de.hybris.platform.commercefacades.product.ProductFacade;
 import de.hybris.platform.commercefacades.product.ProductOption;
 import de.hybris.platform.commercefacades.product.data.ProductReferenceData;
-import de.hybris.platform.core.model.product.ProductModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.List;
+import com.teamidea.platform.technonikol.storefront.controllers.ControllerConstants;
 
 
 /**
@@ -42,21 +48,30 @@ public class ProductCartReferencesComponentController extends AbstractCMSCompone
 	protected static final List<ProductOption> PRODUCT_OPTIONS = Arrays.asList(ProductOption.BASIC, ProductOption.PRICE);
 
 	@Resource(name = "b2bCartFacade")
-    private B2BCartFacade cartFacade;
+	private B2BCartFacade cartFacade;
 
-    @Resource(name = "b2bProductFacade")
-    private ProductFacade productFacade;
+	@Resource(name = "b2bProductFacade")
+	private ProductFacade productFacade;
 
 
 	@Override
 	protected void fillModel(final HttpServletRequest request, final Model model, final ProductReferencesComponentModel component)
 	{
 
-			final List<ProductReferenceData> productReferences = productFacade.getProductReferencesForCode(cartFacade.getSessionCart().getEntries().get(0).getProduct().getCode(),
-					component.getProductReferenceTypes(), PRODUCT_OPTIONS, component.getMaximumNumberProducts());
+		final Set<ProductReferenceData> productReferences = new HashSet<ProductReferenceData>();
+		final List<OrderEntryData> entries = cartFacade.getSessionCart().getEntries();
+		for (final OrderEntryData entry : entries)
+		{
+			final List<ProductReferenceData> oneOfproductReferences = productFacade.getProductReferencesForCode(entry.getProduct()
+					.getCode(), component.getProductReferenceTypes(), PRODUCT_OPTIONS, component.getMaximumNumberProducts());
+			productReferences.addAll(oneOfproductReferences);
+		}
 
-			model.addAttribute("title", component.getTitle());
-			model.addAttribute("productReferences", productReferences);
+		final List<ProductReferenceData> productReferences2 = new ArrayList<ProductReferenceData>();
+		productReferences2.addAll(productReferences);
+
+		model.addAttribute("title", component.getTitle());
+		model.addAttribute("productReferences", productReferences2);
 
 	}
 }
