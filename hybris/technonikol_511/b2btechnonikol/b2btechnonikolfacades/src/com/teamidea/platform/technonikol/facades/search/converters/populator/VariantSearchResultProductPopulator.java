374b4394380462b52f281ef4407271010f160662
Rauf Aliev <aliev@teamidea.ru>
2014-06-21 10:10:41 +0400

    временное изменение на период демонстрации. В поисковых результатах цена берется не из индекса, а из базы. Плохо то, что вытягивается и все остальное тоже, что для производительности нехорошо.

--- hybris/technonikol_511/b2btechnonikol/b2btechnonikolfacades/src/com/teamidea/platform/technonikol/facades/search/converters/populator/VariantSearchResultProductPopulator.java
+++ hybris/technonikol_511/b2btechnonikol/b2btechnonikolfacades/src/com/teamidea/platform/technonikol/facades/search/converters/populator/VariantSearchResultProductPopulator.java
@@ -15,9 +15,20 @@
 package com.teamidea.platform.technonikol.facades.search.converters.populator;
 
 
+import de.hybris.platform.commercefacades.product.PriceDataFactory;
+import de.hybris.platform.commercefacades.product.data.PriceData;
+import de.hybris.platform.commercefacades.product.data.PriceDataType;
 import de.hybris.platform.commercefacades.product.data.ProductData;
 import de.hybris.platform.commercefacades.search.converters.populator.SearchResultProductPopulator;
+import de.hybris.platform.commerceservices.price.CommercePriceService;
 import de.hybris.platform.commerceservices.search.resultdata.SearchResultValueData;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.jalo.order.price.PriceInformation;
+import de.hybris.platform.product.PriceService;
+import de.hybris.platform.product.ProductService;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
 
 
 /**
@@ -27,10 +38,30 @@ import de.hybris.platform.commerceservices.search.resultdata.SearchResultValueDa
  */
 public class VariantSearchResultProductPopulator extends SearchResultProductPopulator
 {
-	@Override
+    @Resource(name = "priceDataFactory")
+    private PriceDataFactory priceDataFactory;
+
+    @Resource(name = "commercePriceService")
+    private CommercePriceService commercePriceService;
+
+    @Resource(name = "priceService")
+    private PriceService priceService;
+
+    @Resource(name = "productService")
+    private ProductService productService;
+
+    @Override
 	public void populate(final SearchResultValueData source, final ProductData target)
 	{
 		super.populate(source, target);
+        final PriceInformation info;
+        final ProductModel productModel = productService.getProductForCode(source.getValues().get("code").toString());
+        info = commercePriceService.getWebPriceForProduct(productModel);
+        if (info != null)
+        {
+               PriceData price = priceDataFactory.create(PriceDataType.BUY, BigDecimal.valueOf(info.getPriceValue().getValue()), info.getPriceValue().getCurrencyIso());
+               target.setPrice(price);
+        }
 		target.setMultidimensional(this.<Boolean> getValue(source, "multidimensional"));
 	}
 }
